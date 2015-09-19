855b56c4e2c5be1dfc94bdb605ed536eba144f38
andrewkrug <andrewkrug@gmail.com>
2015-09-18 18:30:57 -0700

    temporarily remove geohash from venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -5,14 +5,14 @@ class Venue < ActiveRecord::Base
   has_one :event
 
   validates_presence_of :name
-  
-  attr_accessible :name, :address1, :address2, :city, :state, :zip, :time_zone, :lat, :long
-  
-  geocoded_by :full_street_address, :latitude => :lat, :longitude => :long
 
-  after_save :run_geocode, :if => :address1_changed?
+  attr_accessible :name, :address1, :address2, :city, :state, :zip, :time_zone, :lat, :long
+
+  #geocoded_by :full_street_address, :latitude => :lat, :longitude => :long
+
+  #after_save :run_geocode, :if => :address1_changed?
   after_save :refresh_show_stats, :if => :time_zone_changed?
-  
+
   def address_as_string
     street_as_string + " " + city_state_zip_as_string
   end
@@ -21,12 +21,12 @@ class Venue < ActiveRecord::Base
     str = (address1 || "") + " " + (address2 || "")
     str.strip
   end
-  
+
   def city_state_zip_as_string
     str = (city || "") + " " + (state || "") + " " + (zip || "")
     str.strip
   end
-  
+
   def address_as_url_query
     URI::escape(street_as_string + " " + city_state_zip_as_string)
   end
@@ -41,11 +41,11 @@ class Venue < ActiveRecord::Base
       self.event.shows.each {|s| s.delay.refresh_stats }
     end
   end
-  
+
   private
 
     def run_geocode
       Delayed::Job.enqueue(GeocodeAddressJob.new(self))
     end
-  
+
 end
